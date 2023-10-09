cabf559135755200ad7a83f21688d50f9daad6c6
anniekang-dev <hyeju0722@gmail.com>
2023-10-09 07:45:41 -0500

    Delete btn click event

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 const display = document.querySelector('input');
-const numberBtn = document.querySelectorAll('.number');
+const number = document.querySelectorAll('.number');
 const clear = document.querySelector('.all-clear');
+const del = document.querySelector('.delete');
 
 // number button click event handler
-numberBtn.forEach(button => {
+number.forEach(button => {
   button.addEventListener('click', (e) => {
     let value = e.target.innerText;
     display.value += value;
   })
 });
 
+// delete button click event handler
+del.addEventListener('click', () => {
+  display.value = display.value.slice(0, -1);
+});
+
 // display all clear
 clear.addEventListener('click', () => {
   display.value = '';
-})
+});
